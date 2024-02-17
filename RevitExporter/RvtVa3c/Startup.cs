e17c1efaa9ccd07f0c706eae21f0b8b55e4ad223
RyugaRyuzaki <ryuga2005@gmail.com>
2024-02-17 20:37:43 +0700

    create quickly DotBim.addin

---
+++ RevitExporter/RvtVa3c/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace RvtVa3c
+{
+    public class Startup : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication app)
+        {
+
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication app)
+        {
+            string tabName = "DotBim";
+            app.CreateRibbonTab(tabName);
+
+            RibbonPanel ribbonPanel = app.CreateRibbonPanel(tabName, "DotBim Export");
+
+            AddPushButton(ribbonPanel, "Export to .bim", "ExportCmd");
+
+            return Result.Succeeded;
+        }
+
+        private static readonly string Description = "Export revit to .bim";
+        private void AddPushButton(RibbonPanel ribbonPanel, string itemName, string className)
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            string assetsFolderPath = Path.Combine(Path.GetDirectoryName(assemblyPath), "Assets");
+
+            string imagePath = Path.Combine(assetsFolderPath, "DotBim.png");
+
+            PushButtonData pushButtonData = new PushButtonData(itemName, itemName, assemblyPath, $"Revit_Transform.{className}");
+
+            PushButton pushButton = ribbonPanel.AddItem(pushButtonData) as PushButton;
+
+            pushButton.LargeImage = new BitmapImage(new Uri(imagePath));
+
+            pushButton.LongDescription = $"{Description}";
+        }
+    }
+}
